576c7015404e03d8e4003951bf60f771a80dd394
HA-WANG <stone.wwf@gmail.com>
2015-12-06 10:37:39 -0600

    ex13

---
+++ ex13.rb
@@ -0,0 +1,10 @@
+# ARGV call argument variable
+first, second, third = ARGV
+
+puts "The script is called: #{$0}"         # $0 腳本名稱
+puts "Your first variable is: #{first}"
+puts "Your second variable is: #{second}"
+puts "Your third variable is: #{third}"
+
+# ruby ex13.rb 參數1 參數2 參數3
+
